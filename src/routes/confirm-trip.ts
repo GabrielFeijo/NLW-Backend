2106c675b8d63c01e43911e0f7681679f8e6cd9c
Gabriel Feijó <92588394+GabrielFeijo@users.noreply.github.com>
2024-07-10 11:02:27 -0300

    feat: add route for participant to confirm trip attendance

--- src/routes/confirm-trip.ts
+++ src/routes/confirm-trip.ts
@@ -57,7 +57,7 @@ export const confirmTrip = async (app: FastifyInstance) => {
 
 			await Promise.all(
 				trip.participants.map(async (participant) => {
-					const confirmationLink = `http://localhost:3333/trips/${trip.id}/confirm/${participant.id}`;
+					const confirmationLink = `http://localhost:3333/participants/${participant.id}/confirm`;
 
 					const message = await mail.sendMail({
 						from: {
