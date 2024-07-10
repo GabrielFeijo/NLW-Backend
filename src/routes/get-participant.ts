3537191b46bdaff27ee9592fc53d4a55f975c217
Gabriel Feijó <92588394+GabrielFeijo@users.noreply.github.com>
2024-07-10 17:41:49 -0300

    feat: add routes to fetch all participants and fetch a participant by ID

---
+++ src/routes/get-participant.ts
@@ -0,0 +1,38 @@
+import { FastifyInstance } from 'fastify';
+import { ZodTypeProvider } from 'fastify-type-provider-zod';
+import { z } from 'zod';
+import { prisma } from '../lib/prisma';
+
+export const getParticipant = async (app: FastifyInstance) => {
+	app.withTypeProvider<ZodTypeProvider>().get(
+		'/participants/:participantId',
+		{
+			schema: {
+				params: z.object({
+					participantId: z.string().uuid(),
+				}),
+			},
+		},
+		async (request, reply) => {
+			const { participantId } = request.params;
+
+			const participant = await prisma.participants.findUnique({
+				select: {
+					id: true,
+					name: true,
+					email: true,
+					is_confirmed: true,
+				},
+				where: {
+					id: participantId,
+				},
+			});
+
+			if (!participant) {
+				throw new Error('Trip not found');
+			}
+
+			return reply.status(200).send({ participant });
+		}
+	);
+};
