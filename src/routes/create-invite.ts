8c2b1b9cadeb85d698c9b7cd91a3083e125784f4
Gabriel Feijó <92588394+GabrielFeijo@users.noreply.github.com>
2024-07-10 17:43:40 -0300

    feat: add route to create invite

---
+++ src/routes/create-invite.ts
@@ -0,0 +1,87 @@
+import { FastifyInstance } from 'fastify';
+import { ZodTypeProvider } from 'fastify-type-provider-zod';
+import { z } from 'zod';
+import { prisma } from '../lib/prisma';
+import { dayjs } from '../lib/dayjs';
+import { link } from 'fs';
+import { getMailClient } from '../lib/mail';
+import nodemailer from 'nodemailer';
+
+export const createInvite = async (app: FastifyInstance) => {
+	app.withTypeProvider<ZodTypeProvider>().post(
+		'/trips/:tripId/invites',
+		{
+			schema: {
+				params: z.object({
+					tripId: z.string().uuid(),
+				}),
+				body: z.object({
+					email: z.string().email(),
+				}),
+			},
+		},
+		async (request, reply) => {
+			const { tripId } = request.params;
+			const { email } = request.body;
+
+			const trip = await prisma.trip.findUnique({
+				where: {
+					id: tripId,
+				},
+			});
+
+			if (!trip) {
+				throw new Error('Trip not found');
+			}
+
+			const participant = await prisma.participants.create({
+				data: {
+					email,
+					trip_id: tripId,
+				},
+			});
+
+			const formattedStartDate = dayjs(trip.starts_at).format('LL');
+			const formattedEndDate = dayjs(trip.ends_at).format('LL');
+
+			const mail = await getMailClient();
+
+			const confirmationLink = `http://localhost:3333/participants/${participant.id}/confirm`;
+
+			const message = await mail.sendMail({
+				from: {
+					name: 'Equipe plann.er',
+					address: 'suporte@plann.er',
+				},
+				to: participant.email,
+				subject: `Confirme sua presença para ${trip.destination} em ${formattedStartDate}`,
+				html: `<div style="font-family: sans-serif; font-size: 16px; line-height: 1.6">                              
+                        <p>
+                            Você foi convidado(a) para participar de uma viagem para 
+                            <strong>${trip.destination}</strong> nas datas de
+                            <strong>${formattedStartDate} a ${formattedEndDate}</strong>.
+                        </p>
+                        <p/>                                  
+                        <p>Para confirmar sua viagem, clique no link abaixo:</p>
+                        <p/>
+                        <p><a href="${confirmationLink}">Confirmar viagem</a></p>
+                        <p/>
+                        <p>
+                            Caso esteja usando o dispositivo móvel, você também pode confirmar a criação
+                            da viagem pelos aplicativos:
+                        </p>
+                        <p/>
+                        <ul>
+                            <li><a href="#">Aplicativo para iPhone</a></li>
+                            <li><a href="#">Aplicativo para Android</a></li>
+                        </ul>								
+                        <p>Caso você não saiba do que se trata esse e-mail, apenas ignore-o.</p>
+                    </div>`.trim(),
+			});
+
+			console.log(nodemailer.getTestMessageUrl(message));
+
+			return reply.status(201).send({ participant: participant.id });
+		}
+	);
+};
--- src/server.ts
+++ src/server.ts
@@ -8,9 +8,14 @@ import {
 import { confirmTrip } from './routes/confirm-trip';
 import { confirmParticipant } from './routes/confirm-participant';
 import { createActivity } from './routes/create-activity';
-import { getActivities } from './routes/get-links';
+import { getActivities } from './routes/get-activities';
 import { createLink } from './routes/create-link';
-import { getLinks } from './routes/get-activities';
+import { getLinks } from './routes/get-links';
+import { getParticipants } from './routes/get-participants';
+import { createInvite } from './routes/create-invite';
+import { updateTrip } from './routes/update-trip';
+import { getTripDetails } from './routes/get-trip-details';
+import { getParticipant } from './routes/get-participant';
 
 const app = fastify();
 
@@ -23,11 +28,20 @@ app.setSerializerCompiler(serializerCompiler);
 
 app.register(createTrip);
 app.register(confirmTrip);
+app.register(updateTrip);
+app.register(getTripDetails);
+
 app.register(confirmParticipant);
+app.register(getParticipants);
+app.register(getParticipant);
+
 app.register(createActivity);
 app.register(getActivities);
+
 app.register(createLink);
 app.register(getLinks);
+
+app.register(createInvite);
 
 app.listen({ port: 3333 }, (err, address) => {
 	if (err) {
